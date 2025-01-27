ccf60854ef8ce42b56266537466efc0a256d6292
henrotaym <mathieu.henrotay@gmail.com>
2025-01-27 16:30:40 +0000

    🚧 Mysql connector.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import entities.Map;
 import entities.Tile;
 import exceptions.enums.ItemTypeException;
@@ -12,7 +15,16 @@ public class Main {
         Tile tile = new RandomTileFactory().createTile();
         System.out.println(tile.display());
         Map map = new TileListMapFactory().createMap();
-
         map.print();
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://database:3306/example_database",
+                "user",
+                "root"
+            );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
